89b704b72234dc2119134d54e129f6d66161b880
Anorpi <Anorpi@users.noreply.github.com>
2014-05-14 19:53:25 +0800

    Create mygetline.c

---
+++ mygetline.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int  main()
+{
+	int max,c,n,i;
+	max = i = 0;
+	int line[1000];
+	int maxline[1000];
+	while((c=getchar()) != EOF) {
+		
+		if (c == '\n') {
+			line[i] = c; 
+			printf("This line Has %d char \n",i);
+			++i;
+			if (i > max) {
+		 		printf("Max line is: \n");	
+				max = i;
+				for(n=0;n<i;++n) {
+					maxline[n]=line[n];
+					printf("%c",maxline[n]);
+				}
+			}
+			else 
+				{
+					printf("Max line is: \n");
+					for(n=0;n<max;++n) {
+						printf("%c",maxline[n]);
+				}
+
+			}
+				i=0;
+		}
+		else {
+			line[i] = c; 
+			++i;
+			}
+	}
+}
