bbcdedc2d9d63b4b4cbb587ec83fbc36c605e567
Anorpi <Anorpi@users.noreply.github.com>
2014-05-29 15:57:49 +0800

    Create example-1.17.c

---
+++ example-1.17.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int getline(char s1[]);
+
+int main(void)
+{
+	char s1[10];
+	int i,n;
+	while (1) {
+		n = getline(s1);
+		if (n > 80) {
+			printf("Bingo!\n");
+			for (i=0;i<n;++i)
+				putchar(s1[i]);
+			putchar('\n');
+		} else {
+			printf("Bigger 80 will printf\n");
+			printf("%d\n",n);
+		}
+
+	}
+}
+int getline(char s[])
+	{
+		int i,c;
+		for (i = 0;(c = getchar()) != EOF && c != '\n';++i) {
+			s[i] = c;
+		}
+		if (c == '\n') {
+			s[i] = c;
+			++i;
+		}
+		s[i] = '\0';
+
+		return i-1;
+	}
+
+//Not perfect
