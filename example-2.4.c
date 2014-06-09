ef37388506bbe01ab6c4c965bb496af5ff9043fe
Anorpi <Anorpi@users.noreply.github.com>
2014-06-09 13:11:01 +0800

    Update example-2.4.c

--- example-2.4.c
+++ example-2.4.c
@@ -1,36 +1,49 @@
 #include <stdio.h>
+#include <string.h>
 int getline(char s[]);
+void squeeze(char s[],char ss[]);
 int main()
 {
-	int ns,nss;
-	int i,ii,set;
-	set = 0;
 	char s[1000];
 	char ss[1000];
 	while (1) {
-		printf("\nInput your string:\n");
-		ns = getline(s);
-		printf("Input delete string:\n");
-		nss = getline(ss);
+		printf("\nInput your line:\n");
+		getline(s);
+		printf("Input delete line:\n");
+		getline(ss);
+		squeeze(s,ss);
+		printf("After squeeze:\n%s",s);
+	}
+}
+	
+void squeeze(char s[],char ss[])
+{
+	int ns,nss;
+	int i,ii,set,n;
+	set = n = 0;
+		ns = strlen(s);
+		nss = strlen(ss);
 		
 		for (i = 0;i<ns;++i) {
 			
 			for ( ii = 0;ii<nss;++ii) {
 				if (ss[ii] == s[i]) {
 					set = 1;
-					ii = nss;//跳出循环
+					ii = nss;//跳出循环 break?
 					}
 				else {
 					set =0;
 				}
 			}
 			
-			if (set == 0)
-				putchar(s[i]);
+			if (set == 0) {
+				s[n++]=s[i];			
+			}
 		}
-	}
 		
-}		
+		s[n]='\0';	
+	}
+
 int getline(char s[])
 {
 	int i,c;
