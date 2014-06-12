a19b19fb776696f35d1284c7e42499ff825037e5
Anorpi <Anorpi@users.noreply.github.com>
2014-06-12 12:49:04 +0800

    Create example-2.8.c

---
+++ example-2.8.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+unsigned char rightrot(unsigned char x,unsigned char n);
+
+int main()
+{
+	unsigned char x,n;
+	x = 109;
+	
+	for (n = 0;n < 16;++n)
+		printf("should be 182 ,%d\n",rightrot(x,n));
+}
+unsigned char rightrot(unsigned char x,unsigned char n)
+{
+	unsigned char i;
+	unsigned char a,b;
+	n %=8;
+	printf("n is %d\n",n);
+	for (i=0;i<n;++i) {
+		a=x>>1;
+		b=1;
+		b&=x;
+		b<<=7;
+		x=a|b;
+	}
+	
+	return x;
+}
