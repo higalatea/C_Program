4860fc7372f719a5b1ed6b15dc7d137a93d433c3
Anorpi <Anorpi@users.noreply.github.com>
2014-06-12 12:51:18 +0800

    Update example-2.8.c

--- example-2.8.c
+++ example-2.8.c
@@ -7,14 +7,13 @@ int main()
 	x = 109;
 	
 	for (n = 0;n < 16;++n)
-		printf("should be 182 ,%d\n",rightrot(x,n));
+		printf("n is %d\n",rightrot(x,n));
 }
 unsigned char rightrot(unsigned char x,unsigned char n)
 {
 	unsigned char i;
 	unsigned char a,b;
 	n %=8;
-	printf("n is %d\n",n);
 	for (i=0;i<n;++i) {
 		a=x>>1;
 		b=1;
