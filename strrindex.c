1525fedbbb365ceb14a1f58549e2f12f00b0de1a
Anorpi <Anorpi@users.noreply.github.com>
2014-06-30 00:09:24 +0800

    Update strrindex.c

--- strrindex.c
+++ strrindex.c
@@ -3,10 +3,10 @@ int strrindex(char s[], char t[])
 {
 	int i, j, k, m = -1;
         for (i = 0; s[i] != '\0'; i++) {
-			for (j=i, k=0; t[k]!='\0' && s[j]==t[k]; j++, k++)
+			for (j=i, k=0; t[k]!='\0' && s[j]==t[k]; j++, k++) //寻找s中的t字符串
 				;
-			if (k > 0 && t[k] == '\0')
+			if (k > 0 && t[k] == '\0')//m存储s中每次出现t字符串的位置
 				m = i;
 		}
-        return m;
+        return m;//返回最后一次获得的m值，即t在s中最右边出现的位置
 }
